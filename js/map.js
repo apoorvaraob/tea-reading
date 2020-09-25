13eee8f35da3ac5305160155157d67e59be9fef5
Cecilia Tran <cecitran94@gmail.com>
2020-09-25 16:13:26 -0700

    added audio

--- js/map.js
+++ js/map.js
@@ -11,12 +11,21 @@ map.addEventListener("mousemove", function(event){
     moveLight(event);
 });
 
+//setup the mp3 to play on click on the pin
+var music = new Audio();
+function playMusic(file) {
+    music.pause();
+    music = new Audio(file);
+    music.volume = 0.1;
+    music.play();
+}
 
+//pin click -> show tea modal
 let teaModal = document.querySelector(".tea-container-modal");
-//pin click show modal
 let pins = document.querySelectorAll(".pin");
 pins.forEach(pin => {
     pin.addEventListener("click", function(){
+        playMusic("../tea-reading/misc/magic-chime.mp3");
         teaModal.style.display = "block";
         setTimeout(function(){
             teaModal.style.opacity = 1;
